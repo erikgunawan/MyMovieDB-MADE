1736e4bed1c691c35e23d7b91b3c3eff9203e77b
erikgunawan <erik.gunawa@alfadigital.id>
2021-01-05 11:34:56 +0700

    update some dependencies implement base activity

---
+++ app/src/main/java/id/ergun/mymoviedb/ui/view/base/BaseActivity.kt
@@ -0,0 +1,39 @@
+package id.ergun.mymoviedb.ui.view.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+/**
+ * Created by alfacart on 15/12/20.
+ */
+abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
+    protected open var binding: V? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getInflatedLayout(layoutInflater))
+        setup()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.binding = null
+    }
+
+    //internal functions
+    private fun getInflatedLayout(inflater: LayoutInflater): View {
+        this.binding = setBinding(inflater)
+
+
+        return binding?.root
+            ?: error("Please add your inflated binding class instance")
+    }
+
+    //abstract functions
+    abstract fun setBinding(layoutInflater: LayoutInflater): V
+
+    abstract fun setup()
+}
